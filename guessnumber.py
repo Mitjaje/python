ce79d091cb85930fff29091cb092a0b522881de7
Mitja Jereb <mjereb@gmail.com>
2016-03-21 23:07:42 +0100

    input type check

--- guessnumber.py
+++ guessnumber.py
@@ -1,14 +1,25 @@
 number = 3
 inputnumber = 0
+error = 0
 
-while number != inputnumber :
-    inputnumber = input("Please try to guesss a number between 0 and 10: ")
-    if inputnumber <= 10 and inputnumber >= 0 :
-        if inputnumber < number :
-            print("Try higher!")
-        elif inputnumber > number :
-            print("Try lower!")
+while number != int(inputnumber) :
+
+    inputnumber = raw_input("\nPlease try to guesss a number between 0 and 10: ")
+    try:
+        val = int(inputnumber)
+    except ValueError:
+        print("\nOops, %s is not a number.." % inputnumber)
+        error = 1
+        break
+    if int(inputnumber) <= 10 and int(inputnumber) >= 0 :
+        if int(inputnumber) < number :
+            print("\nTry higher!")
+        elif int(inputnumber) > number :
+            print("\nTry lower!")
     else :
-        print("Stay between 0 and 10!")
-print("Bingo!")
+        print(" \nIsn't between 0 and 10 hard enough??")
+if error == 1 :
+    error = 1
+else :
+    print("\n Bingo!")
 
