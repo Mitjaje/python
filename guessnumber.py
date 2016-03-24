90304f7438863485c86dcd88189c821ceb7b3239
Mitja Jereb <mjereb@gmail.com>
2016-03-24 10:01:11 +0100

    simplified version

--- guessnumber.py
+++ guessnumber.py
@@ -1,25 +1,20 @@
 number = 3
 inputnumber = 0
-error = 0
 
-while number != int(inputnumber) :
+while number != int(inputnumber):
 
     inputnumber = raw_input("\nPlease try to guesss a number between 0 and 10: ")
     try:
-        val = int(inputnumber)
+        value = int(inputnumber)
     except ValueError:
         print("\nOops, %s is not a number.." % inputnumber)
-        error = 1
         break
-    if int(inputnumber) <= 10 and int(inputnumber) >= 0 :
-        if int(inputnumber) < number :
+    if value <= 10 and value >= 0:
+        if value < number:
             print("\nTry higher!")
-        elif int(inputnumber) > number :
+        elif value > number:
             print("\nTry lower!")
-    else :
+        elif value == number:
+            print("\n Bingo!")
+    else:
         print(" \nIsn't between 0 and 10 hard enough??")
-if error == 1 :
-    error = 1
-else :
-    print("\n Bingo!")
-
